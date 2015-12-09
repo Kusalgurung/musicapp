5709ebcad9aeb4380a635c323fe5112e762fc605
kusalgurung <grgkusal@hotmail.com>
2015-12-09 12:27:11 +0000

    added index to artists and  welcome seed

--- db/migrate/20151209121420_create_artists.rb
+++ db/migrate/20151209121420_create_artists.rb
@@ -1,7 +1,7 @@
 class CreateArtists < ActiveRecord::Migration
   def change
     create_table :artists do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :biography
 
       t.timestamps null: false
